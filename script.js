62195a11d6bed114cf737589b6c7d638d13aff55
Gowtham-Mani-24 <workspaceofgowtham@gmail.com>
2024-07-16 12:00:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -17,6 +17,23 @@ function input_create(tagname,attrname,attrvalue,attrname1,attrvalue1){
     return input_ele;
 }
 
+function button_create(tagname,attrname2,attrvalue2,methodname,methodvalue,content){
+    var button_ele = document.createElement(tagname);
+    button_ele.setAttribute(attrname2,attrvalue2);
+    button_ele.setAttribute(methodname,methodvalue);
+    button_ele.innerHTML = content;
+    return button_ele;
+}
+
+function result(){
+    var res = document.getElementById("fname").value;
+    var res1 = document.getElementById("mname").value;
+    var res2 = document.getElementById("lname").value;
+    var res3 = document.getElementById("email").value;
+    var res4 = document.getElementById("pass").value;
+    console.log(res,res1,res2,res3,res4);
+}
+
 var fname = label_create("label","for","fname","FirstName");
 var line_break = br_create();
 var fname_input = input_create("input","type","fname","id","fname");
@@ -39,5 +56,8 @@ document.body.append(email,line_break,email_input,br_create());
 
 var pass = label_create("label","for","pass","Password");
 var line_break = br_create();
-var pass_input = input_create("input","type","pass","id","pass");
+var pass_input = input_create("input","type","password","id","pass");
 document.body.append(pass,line_break,pass_input,br_create());
+
+var submit_input = button_create("button","type","button","onclick","result()","Submit");
+document.body.append(submit_input);
